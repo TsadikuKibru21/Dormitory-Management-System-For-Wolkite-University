763a0e3317b0a0cba4c67822cabefb454a67de13
TsadikuKibru21 <tsadikuk@gmail.com>
2024-05-10 12:41:13 +0300

    h

--- DMS/settings.py
+++ DMS/settings.py
@@ -57,6 +57,7 @@ CRISPY_ALLOWED_TEMPLATE="bootstrap5"
 CRISPY_TEMPLATE_PACK="bootstrap5"
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
